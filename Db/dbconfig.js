e375d902a8b9d04786cfef3efb9a1c69d8ff8462
niteshkumar257 <niteshreddy257@gmail.com>
2024-05-19 01:08:36 +0530

    hosted pg database connected

--- Db/dbconfig.js
+++ Db/dbconfig.js
@@ -1,19 +1,26 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const { Client } = pg;
 const client = new Client({
-  user: "postgres",
-  password: "nitesh",
-  host: "localhost",
-  port: "5432",
-  database: "guestara",
+  user:process.env.PG_USER_NAME,
+  password: process.env.PG_HOSTED_PASSWORD,
+  host: process.env.PG_HOST_NAME,
+  port: process.env.PG_HOST_PORT,
+  database:process.env.PG_USER_DATABASE,
+  ssl:true
+ 
 });
 
 const connectDb = async () => {
   try {
-    await client.connect();
+   let res= await client.connect();
+   console.log(res);
     console.log("Connected to the database");
   } catch (error) {
+    console.log(error);
     console.error("Error connecting to the database:", error.message);
   }
 };
