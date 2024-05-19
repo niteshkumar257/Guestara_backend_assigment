ca3cdd5be161648b29f0e6a2edaeeca4b6e465cc
niteshkumar257 <niteshreddy257@gmail.com>
2024-05-19 21:23:01 +0530

    bug fix in cloudinary

--- Utils/cloudinar_image_upload.js
+++ Utils/cloudinar_image_upload.js
@@ -2,6 +2,8 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 import dotenv from "dotenv";
 
+dotenv.config();
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key:process.env.CLOUDINARY_CLOUD_API_KEY,
